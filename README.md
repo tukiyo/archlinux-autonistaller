329a621da506656630d7fa9dc622260b1bd1b902
tukiyo3 <tukiyo3@gmail.com>
2015-02-07 19:55:55 +0900

    macmini2014-MGEQ2J用作った

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # archlinux-autonistaller
+
+macmini 2014のarchlinuxインストールを自動化したかった。
+
+# 使い方
+
+インターネットに接続できること。
+USBメモリからarchlinuxを起動させて実行するだけ。
+
+sh macmini2014-MGEQ2J.sh
+
+途中rootユーザのパスワード設定する入力があるので入力。
+全部終わったら再起動で完了。
---
+++ macmini2014-MGEQ2J.sh
@@ -0,0 +1,142 @@
+#!/bin/sh
+set -ux
+
+#----------------------
+# ネットワーク設定用
+#----------------------
+BOND0='192.168.100.2/24'
+GATEWAY='192.168.100.1'
+DNS1='8.8.8.8'
+DNS2='8.8.4.4'
+BINDSTOINTERFACES="enp3s0f0"
+
+#----------------------
+# partitioning /dev/sda
+# * sda1 : EFI Filesystem : 100MB
+# * sda2 : SWAP : 4GB
+# * sda3 : xfs : 残り
+#----------------------
+gdisk /dev/sda <<EOF
+o
+y
+n
+1
+
++100M
+EF00
+n
+2
+
++4G
+8200
+n
+3
+
+
+
+
+w
+y
+EOF
+
+
+#----------------------
+# partitioning /dev/sdb
+# * sdb1 : xfs : /
+#----------------------
+gdisk /dev/sdb <<EOF
+o
+y
+n
+1
+
+
+
+w
+y
+EOF
+
+#-------------
+# format
+#-------------
+mkfs.vfat -F32 /dev/sda1
+mkswap /dev/sda2
+mkfs.xfs -f /dev/sda3
+mkfs.xfs -f /dev/sdb1
+
+#-------------
+# mount
+#-------------
+mount /dev/sdb1 /mnt
+mkdir -p /mnt/boot/efi
+mount /dev/sda1 /mnt/boot/efi
+
+#-------------
+# mirror
+#-------------
+cat > /etc/pacman.d/mirrorlist <<EOF
+Server = http://ftp.tsukuba.wide.ad.jp/Linux/archlinux/\$repo/os/\$arch
+EOF
+
+#-------------
+# pacstrap
+#-------------
+pacstrap /mnt \
+  base base-devel vim openssh grub efibootmgr os-prober
+
+#-------------
+# genfstab
+#-------------
+genfstab -U -p /mnt >> /mnt/etc/fstab
+
+#-------------
+# chroot
+#-------------
+CHROOT="arch-chroot /mnt"
+
+$CHROOT sed -i -e 's/#ja_JP.UTF-8/ja_JP.UTF-8/' /etc/locale.gen
+$CHROOT sed -i -e 's/#en_US.UTF-8/en_US.UTF-8/' /etc/locale.gen
+echo LANG=ja_JP.UTF-8 > /mnt/etc/locale.conf
+$CHROOT locale-gen
+echo KEYMAP=jp106 > /mnt/etc/vconsole.conf
+
+$CHROOT ln -s /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
+$CHROOT hwclock --systohc --utc
+$CHROOT echo Arch >  /etc/hostname
+
+$CHROOT systemctl enable sshd
+$CHROOT mkinitcpio -p linux
+$CHROOT passwd
+
+$CHROOT grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=arch_grub --recheck
+$CHROOT grub-mkconfig -o /boot/grub/grub.cfg
+
+#-------------
+# network
+#-------------
+$CHROOT pacman -S --noconfirm netctl ifenslave
+echo "net.ipv4.ip_forward=1" > /mnt/etc/sysctl.d/99-sysctl.conf
+
+cat > /mnt/etc/netctl/bond0 <<EOF
+Description='Bond0 Interface'
+Interface='bond0'
+Connection=bond
+BindsToInterfaces=($BINDSTOINTERFACES)
+IP=static
+Address=('$BOND0')
+Gateway='$GATEWAY'
+DNS=('$DNS1' '$DNS2')
+EOF
+cat > /mnt/etc/modprobe.d/bonding.conf <<EOF
+options bonding miimon=100
+options bonding mode=active-backup
+EOF
+$CHROOT netctl enable bond0
+
+#-------------
+# finish
+#-------------
+set +x
+echo "--------------------------------"
+echo "install finished. prease reboot."
+echo "--------------------------------"
